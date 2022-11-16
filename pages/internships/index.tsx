682eea4e275563a996e7099d17f370f1baa1f50a
Jacob <jacob.heim@stud-mail.uni-wuerzburg.de>
2022-11-16 01:59:06 +0100

    no local build errors

--- pages/internships/index.tsx
+++ pages/internships/index.tsx
@@ -1,20 +1,31 @@
-import React from 'react';
+import React from "react";
 import Fullpage from "../../components/Fullpage";
-import WorkHeader from "../../components/work/WorkHeader";
-import InternShipTile from "./InternShipTile";
+import InternShipTile from "../../components/internship/InternShipTile";
 
 function Index() {
     return (
         <Fullpage>
             <div className="max-w-5xl p-12">
-                <h1 className="font-serif text-3xl text-center sm:text-5xl leading-relaxed">Internshipts reports</h1>
-                <p className="py-8 text-xl">Here, you can take a look at my report of my finished internships.</p>
+                <h1 className="text-center font-serif text-3xl leading-relaxed sm:text-5xl">
+                    Internshipts reports
+                </h1>
+                <p className="py-8 text-xl">
+                    Here, you can take a look at my report of my finished
+                    internships.
+                </p>
             </div>
-            <div className="w-full h-full border-t border-primary">
-                <div className="mx-auto grid grid-cols-1 md:grid-cols-2 gap-4 max-w-7xl h-full justify-items-center p-12">
-                    <InternShipTile link={"/zb/zb_report.pdf"} filename={"Praktikumszeignis Zukunftsbauer.pdf"} title={"Die Zukunftsbauer"}/>
-                    <InternShipTile title={"MAW Production"} link={"/maw/maw.pdf"} filename={"MAW Praktikumsbescheinigung"}/>
-
+            <div className="h-full w-full border-t border-primary">
+                <div className="mx-auto grid h-full max-w-7xl grid-cols-1 justify-items-center gap-4 p-12 md:grid-cols-2">
+                    <InternShipTile
+                        link={"/zb/zb_report.pdf"}
+                        filename={"Praktikumszeignis Zukunftsbauer.pdf"}
+                        title={"Die Zukunftsbauer"}
+                    />
+                    <InternShipTile
+                        title={"MAW Production"}
+                        link={"/maw/maw.pdf"}
+                        filename={"MAW Praktikumsbescheinigung"}
+                    />
                 </div>
             </div>
         </Fullpage>
